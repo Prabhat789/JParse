75112e4e2e1349947960cf960aa0c4c695e40bda
Prabhat Tiwari <prabhat.kumar.ec@gmail.com>
2024-05-17 14:02:29 +0530

    Removed Unused imports and fixed the buildJsonFromModel fun.

--- jparse/src/main/java/com/emittoz/jparse/parser/ParseJson.kt
+++ jparse/src/main/java/com/emittoz/jparse/parser/ParseJson.kt
@@ -1,21 +1,19 @@
 package com.emittoz.jparse.parser
 
 import com.emittoz.jparse.parser.FunctionProvider.Companion.FUNC_FROM_JSON
+import com.emittoz.jparse.parser.FunctionProvider.Companion.FUNC_TO_JSON
 import com.emittoz.jparse.parser.FunctionUtils.Companion.CLOSE_BRACE
 import com.emittoz.jparse.parser.FunctionUtils.Companion.DOT
+import com.emittoz.jparse.parser.FunctionUtils.Companion.INDENTATION_12
 import com.emittoz.jparse.parser.FunctionUtils.Companion.JSON_OBJECT
 import com.emittoz.jparse.parser.FunctionUtils.Companion.OPEN_BRACE
 import com.emittoz.jparse.processor.JParseProcessor.Companion.NAME_PREFIX
 import com.google.devtools.ksp.symbol.KSPropertyDeclaration
 import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.Dynamic
-import com.squareup.kotlinpoet.LambdaTypeName
 import com.squareup.kotlinpoet.ParameterizedTypeName
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeName
-import com.squareup.kotlinpoet.TypeVariableName
-import com.squareup.kotlinpoet.WildcardTypeName
 import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.asTypeName
 
@@ -73,16 +71,29 @@ class ParseJson {
 
     fun buildJsonFromModel(prop: KSPropertyDeclaration): String {
         val propName = prop.simpleName.asString()
-        val propType = prop.type.resolve().declaration.toTypeName()
-        return when(propType) {
-            is ClassName -> {}
-            Dynamic -> {}
-            is LambdaTypeName -> {}
-            is ParameterizedTypeName -> {}
-            is TypeVariableName -> {}
-            is WildcardTypeName -> {}
-            else -> {}
-        }.toString()
+        return when(val propType = prop.type.resolve().declaration.toTypeName()) {
+            String::class.asTypeName() -> {
+                putPrimitiveVariableInJsonObject(propName)
+            }
+            Int::class.asTypeName() -> {
+                putPrimitiveVariableInJsonObject(propName)
+            }
+            Boolean::class.asTypeName() -> {
+                putPrimitiveVariableInJsonObject(propName)
+            }
+            Long::class.asTypeName() -> {
+                putPrimitiveVariableInJsonObject(propName)
+            }
+            Double::class.asTypeName() -> {
+                putPrimitiveVariableInJsonObject(propName)
+            }
+            List::class.asTypeName() -> {
+                ""//"${getList(propName, propType)}"
+            }
+            else -> {
+                putObjectInJsonObject(propName, getClassName(propType))
+            }
+        }
     }
 
     private fun getJsonElementValue(elementName: String, propertyName: String): String {
@@ -91,6 +102,14 @@ class ParseJson {
 
     private fun getObject(propertyName: String, typeName: TypeName): String {
         return "${NAME_PREFIX}${getClassName(typeName)}()${DOT}${FUNC_FROM_JSON}(${OPT_JSON_OBJECT}(\"$propertyName\"))"
+    }
+
+    private fun putPrimitiveVariableInJsonObject(propName: String): String {
+        return "${INDENTATION_12}put(\"$propName\", $propName)"
+    }
+    private fun putObjectInJsonObject(propName: String, objectName: String): String {
+        return "${INDENTATION_12}put(\"$propName\", " +
+                "$NAME_PREFIX$objectName()$DOT$FUNC_TO_JSON($propName))"
     }
 
     private fun getList(propertyName: String, typeName: TypeName): PropertySpec {
