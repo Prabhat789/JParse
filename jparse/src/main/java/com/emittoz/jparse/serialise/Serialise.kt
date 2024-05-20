7f8f2ef7a2973252dcd9ae80ca57872c077d21b1
Prabhat Tiwari <prabhat.kumar.ec@gmail.com>
2024-05-20 08:31:05 +0530

    Added parsing function to the interface.

--- jparse/src/main/java/com/emittoz/jparse/serialise/Serialise.kt
+++ jparse/src/main/java/com/emittoz/jparse/serialise/Serialise.kt
@@ -1,5 +1,14 @@
 package com.emittoz.jparse.serialise
 
+import org.json.JSONObject
+
 interface Serialise<T> {
 
+    fun fromJson(json: String?): T
+
+    fun fromJson(jsonObject: JSONObject?): T
+
+    fun toJson(model: T?): JSONObject
+
+    fun toJsonString(model: T?): String
 }
